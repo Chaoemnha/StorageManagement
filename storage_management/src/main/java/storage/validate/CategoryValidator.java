84cab1419a2745cd45aea5f560e5e1c531db2673
Chaoemnha <luannguyentm99@gmail.com>
2025-07-20 14:15:24 +0700

    Fix

--- storage_management/src/main/java/storage/validate/CategoryValidator.java
+++ storage_management/src/main/java/storage/validate/CategoryValidator.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import storage.model.Category;
 import storage.service.ProductService;
