c2ddd9f027ae021adf482d8f9c74869feee623b4
Chaoemnha <luannguyentm99@gmail.com>
2025-07-23 14:30:25 +0700

    Sua edit co the cho trung

--- storage_management/src/main/java/storage/validate/RegisterValidator.java
+++ storage_management/src/main/java/storage/validate/RegisterValidator.java
@@ -11,28 +11,32 @@ import org.springframework.validation.Validator;
 
 import storage.model.User;
 import storage.service.UserService;
+
 @Component
-public class RegisterValidator implements Validator{
+public class RegisterValidator implements Validator {
 	@Autowired
 	private UserService userService;
+
 	public boolean supports(Class<?> clazz) {
 		return clazz == User.class;
 	}
-	
+
 	public void validate(Object target, Errors errors) {
 		User user = (User) target;
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "userName", "msg.required");
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "msg.required");
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "email", "msg.required");
-		if(StringUtils.hasLength(user.getUserName())&& StringUtils.hasLength(user.getPassword())&&StringUtils.hasLength(user.getEmail())) {
+		if (StringUtils.hasLength(user.getUserName()) && StringUtils.hasLength(user.getPassword())
+				&& StringUtils.hasLength(user.getEmail())) {
 			List<User> users = userService.findByProperty("userName", user.getUserName());
 			List<User> userz = userService.findByProperty("email", user.getEmail());
-			if(users!=null && !users.isEmpty()) {
-				errors.rejectValue("userName", "msg.username.exists");
-			}
-				else if(userz!=null && !userz.isEmpty()) {
+			if (users != null && !users.isEmpty()) {
+				if (users.get(0).getId() != user.getId())
+					errors.rejectValue("userName", "msg.username.exists");
+			} else if (userz != null && !userz.isEmpty()) {
+				if (users.get(0).getId() != user.getId())
 					errors.rejectValue("email", "msg.email.exists");
-				}
+			}
 		}
 	}
 }
